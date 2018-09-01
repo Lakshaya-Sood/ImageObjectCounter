a8acdd2daf2ac0c66a327f9cf70e27f882c4db5d
lakshaya.sood <lakshaya.sood@metronom-external.com>
2018-09-01 15:30:33 +0530

    updating pip requirement.txt

--- server/server.py
+++ server/server.py
@@ -2,7 +2,6 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from insights import getInsightsOnImage, imageCheckTollBooth
 import os
-import shutil
 import logging
 import base64
 import json
